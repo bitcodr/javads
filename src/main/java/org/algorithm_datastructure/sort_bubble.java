84c426b3b02a177bc8613f57d800da4d58d7d677
Amir Roshanaei <bitcodr@gmail.com>
2023-09-06 13:53:22 +0200

    improve bubble sort

--- src/main/java/org/algorithm_datastructure/sort_bubble.java
+++ src/main/java/org/algorithm_datastructure/sort_bubble.java
@@ -11,11 +11,11 @@ public class sort_bubble {
 
 
     private static int[] sort(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[j] < nums[i]) {
-                    int temp = nums[i];
-                    nums[i] = nums[j];
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = 0; j < nums.length - i - 1; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    int temp = nums[j+1];
+                    nums[j+1] = nums[j];
                     nums[j] = temp;
                 }
             }
